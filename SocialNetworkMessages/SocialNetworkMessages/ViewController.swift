b65f6ff18a498647585be89204d03b517622aba2
Artem Burkan <job.artem.burkan@gmail.com>
2019-05-19 22:12:22 +0700

    Добавлен проект

---
+++ SocialNetworkMessages/SocialNetworkMessages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialNetworkMessages
+//
+//  Created by Artyom Burkan on 19/05/2019.
+//  Copyright © 2019 Artyom Burkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
